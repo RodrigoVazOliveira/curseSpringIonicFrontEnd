48adaf70b6208525c9a2e347d134b2508c22b5e4
Rodrigo Vaz de Oliveira <rodrigovazdeoliveira@gmail.com>
2019-09-08 13:12:51 -0300

    Estado e cidade

--- cursomc/src/main/java/com/rodrigovazdeveloper/cursomc/CursomcApplication.java
+++ cursomc/src/main/java/com/rodrigovazdeveloper/cursomc/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.rodrigovazdeveloper.cursomc.domain.Categoria;
+import com.rodrigovazdeveloper.cursomc.domain.Cidade;
+import com.rodrigovazdeveloper.cursomc.domain.Estado;
 import com.rodrigovazdeveloper.cursomc.domain.Produto;
 import com.rodrigovazdeveloper.cursomc.repositories.CategoriaRepository;
+import com.rodrigovazdeveloper.cursomc.repositories.CidadeRepository;
+import com.rodrigovazdeveloper.cursomc.repositories.EstadoRepository;
 import com.rodrigovazdeveloper.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class CursomcApplication  implements CommandLineRunner {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -45,9 +55,22 @@ public class CursomcApplication  implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().add(cat1);
 		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2,c3));
+		
 		// preenchimento automatico das instâncias
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
-		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));	
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
+		  
 		
 	}
 
--- cursomc/src/main/java/com/rodrigovazdeveloper/cursomc/resources/CategoriaResource.java
+++ cursomc/src/main/java/com/rodrigovazdeveloper/cursomc/resources/CategoriaResource.java
@@ -1,7 +1,5 @@
 package com.rodrigovazdeveloper.cursomc.resources;
 
-import java.util.ArrayList;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
