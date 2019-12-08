17c570a2b8bd8f146bdf673fc883453c6c4b0fd3
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2019-12-07 21:54:27 -0300

    Extraindo o email do token e armazenando-o em localStorage

--- ws-ionic/curseSpringIonic/src/app/services/auth.service.ts
+++ ws-ionic/curseSpringIonic/src/app/services/auth.service.ts
@@ -4,10 +4,13 @@ import { HttpClient } from '@angular/common/http';
 import { API_CONFIG } from '../config/api.config';
 import { localUser } from '../models/local.user';
 import { StorageService } from './storage.service';
+import { JwtHelperService } from "@auth0/angular-jwt";
 
 @Injectable()
 
 export class AuthService {
+
+    jwtHelper = new JwtHelperService();
 
     constructor(public http: HttpClient,
             public storage: StorageService) {
@@ -30,7 +33,8 @@ export class AuthService {
 
         let tok = authorizationValue.substring(7);
         let user: localUser = {
-            token: tok
+            token: tok,
+            email: this.jwtHelper.decodeToken(tok).sub
         }
 
         this.storage.setLocalUser(user);
