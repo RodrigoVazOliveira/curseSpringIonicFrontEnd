f9df363c7b03c7400eb4cf8c563832f201ddb59a
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2019-12-22 08:21:27 -0300

    Criando um formGroup para controlar o formulário

--- ws-ionic/curseSpringIonic/src/app/signup/signup.page.ts
+++ ws-ionic/curseSpringIonic/src/app/signup/signup.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -10,7 +10,85 @@ export class SignupPage implements OnInit {
 
   formGroup: FormGroup
 
-  constructor() { }
+  constructor(public formBuilder: FormBuilder) {
+    this.formGroup = this.formBuilder.group({
+      nome: [
+        'Joaquim',[
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(120)
+        ]
+       ],
+       email: [
+         'joaquim@exemplo.com.br',[
+           Validators.required, 
+          Validators.email
+        ]
+       ],
+       tipo: [
+         '1', [
+           Validators.required
+         ]
+       ],
+       cpfOuCpnj: [
+         '22248638027', [
+           Validators.required, 
+           Validators.minLength(11), 
+           Validators.maxLength(14)
+         ]
+       ],
+       senha: [
+         '123',[
+           Validators.required
+         ]
+       ],
+       logradouro: [
+         'Rua Egidio',[
+           Validators.required
+         ]
+       ],
+       numero: [
+         '123',[
+           Validators.required
+         ]
+       ],
+       complemento: [
+         'APTO 1',
+         []
+       ],
+       bairro: [
+         'Copacabana', [
+           Validators.required
+         ]
+       ],
+       cep: [
+         '10828333', [
+           Validators.required
+         ]
+       ],
+       telefone1: [
+         '977261827', [
+           Validators.required
+         ]
+       ],
+       telefone2: [
+        '', []
+      ],
+      telefone3: [
+        '', []
+      ],
+      estadoId: [
+        null, [
+          Validators.required
+        ]
+      ],
+      CidadeId: [
+        null, [
+          Validators.required
+        ]
+      ]
+    });
+   }
 
   ngOnInit() {
   }
